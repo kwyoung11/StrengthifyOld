7c38f0fce6bf9745e0298e2d058f47b3340d81ba
Kevin Young <kyoung18@terpmail.umd.edu>
2013-04-10 13:06:13 -0400

    validations for workout and exercise models

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,4 +2,10 @@ class Exercise < ActiveRecord::Base
   attr_accessible :name, :weight, :reps
   
   belongs_to :workout
+  
+  
+  validates :name, presence: true
+  validates :weight, presence: true
+  validates :reps, presence: true
+  
 end
