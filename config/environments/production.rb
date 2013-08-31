3149beece29c9d533a76217440e2cbbaa028e3ce
Kevin Young <kyoung18@terpmail.umd.edu>
2013-08-31 18:41:28 -0400

    fixing hostname in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Cs50xFinalProject::Application.configure do
   
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
-    config.action_mailer.default_url_options = { :host => 'fierce-tundra-1121.herokuapp.com' }
+    config.action_mailer.default_url_options = { :host => 'strengthify.com' }
     config.action_mailer.smtp_settings = {
       :address => 'smtp.gmail.com',
       :port => 587,
