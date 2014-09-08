15adf986322987c9712f45f2ba791c9196bdfd81
Kevin Young <kyoung18@terpmail.umd.edu>
2014-09-08 00:54:06 -0400

    link_to_function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,6 +15,17 @@ module ApplicationHelper
      end
      link_to(name, '#', id: link_id, class: "workout-add-exercise-link", data: {id: id, fields: fields.gsub("\n", "")})
   end
+
+  # replace removed link_to_function in rails4
+  def link_to_function(name, *args, &block)
+     html_options = args.extract_options!.symbolize_keys
+
+     function = block_given? ? update_page(&block) : args[0] || ''
+     onclick = "#{"#{html_options[:onclick]}; " if html_options[:onclick]}#{function}; return false;"
+     href = html_options[:href] || '#'
+
+     content_tag(:a, name, html_options.merge(:href => href, :onclick => onclick))
+  end
    
   def link_to_submit(text, id = nil)
    id ||= "submit_link"
