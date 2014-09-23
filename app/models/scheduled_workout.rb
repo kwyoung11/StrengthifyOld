1a660153412a65f3761bf89e7ac8aabd509b4d30
Kevin Young <kyoung18@terpmail.umd.edu>
2014-09-23 00:34:07 -0400

    ask_for_workout_confirmation fix

--- app/models/scheduled_workout.rb
+++ app/models/scheduled_workout.rb
@@ -6,14 +6,14 @@ class ScheduledWorkout < ActiveRecord::Base
 	has_many :users, through: :scheduled_workout_participations
 
 
-	def send_reminders
+	def self.send_reminders
 		@scheduled_workouts = ScheduledWorkout.where(scheduled_date: (Time.now.midnight - 1.day)..Time.now.midnight)
 		@scheduled_workouts.each do |sw|
 			UserMailer.workout_reminder(User.find(sw.scheduled_workout_participations.user_id), sw).deliver
 		end
 	end
 
-	def ask_for_workout_confirmations
+	def self.ask_for_workout_confirmations
 		@scheduled_workouts = ScheduledWorkout.where(scheduled_date: (Time.now.midnight - 2.day)..Time.now.midnight - 1.day)
 		@scheduled_workouts.each do |sw|
 			UserMailer.ask_for_workout_confirmations(User.find(sw.scheduled_workout_participations.user_id), sw).deliver
