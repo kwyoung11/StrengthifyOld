c7eea636257ecf9d3d97f8e5fd5b84e4afbb97cc
Kevin Young <kyoung18@terpmail.umd.edu>
2014-09-23 17:58:02 -0400

    adding production keys for moves integration

--- config/initializers/moves.rb
+++ config/initializers/moves.rb
@@ -2,5 +2,12 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 	configure do |config|
     config.path_prefix = '/auth'
   end
-  provider :moves, ENV['MOVES_KEY'], ENV['MOVES_SECRET']
+
+  # switch between dev and prod
+  if Rails.env.production?
+  	provider :moves, ENV['MOVES_KEY_PROD'], ENV['MOVES_SECRET_PROD']
+  else
+  	provider :moves, ENV['MOVES_KEY'], ENV['MOVES_SECRET']
+  end
+
 end
